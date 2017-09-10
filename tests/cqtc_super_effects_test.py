b86ba501007dc917baf8a05614582708c27b6008
Crul <rauljavier.vila@gmail.com>
2017-09-10 23:02:56 +0200

    refactor single quotes > double quotes

--- tests/cqtc_super_effects_test.py
+++ tests/cqtc_super_effects_test.py
@@ -2,9 +2,9 @@ from unittest import mock
 
 class TestCqtcSuperEffects():
 
-	@mock.patch('bpy.props.IntProperty')
-	@mock.patch('bpy.props.FloatProperty')
-	@mock.patch('bpy.props.BoolProperty')
+	@mock.patch("bpy.props.IntProperty")
+	@mock.patch("bpy.props.FloatProperty")
+	@mock.patch("bpy.props.BoolProperty")
 	def test_properties(self, MockBoolProperty, MockFloatProperty, MockIntProperty):
 		from cqtc_super_effects.super_effect_properties import SuperEffectProperties
 		
--- tests/cqtc_tools_test.py
+++ tests/cqtc_tools_test.py
@@ -2,8 +2,8 @@ from unittest import mock
 
 class TestCqtcTools():
 
-	@mock.patch('bpy.props.IntProperty')
-	@mock.patch('bpy.props.BoolProperty')
+	@mock.patch("bpy.props.IntProperty")
+	@mock.patch("bpy.props.BoolProperty")
 	def test_zoom_properties(self, MockBoolProperty, MockIntProperty):
 		from cqtc_tools.set_proxy_config_properties import SetProxyConfigProperties
 		
